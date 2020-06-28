2f0779d54d4f105dba11bf184a347eaaee2dd6fb
Chris Campbell <campbellC@users.noreply.github.com>
2020-06-28 15:33:51 +0100

    Move Chrome testing code into subfolder

--- test_against_chrome.sh
+++ test_against_chrome.sh
@@ -9,10 +9,10 @@ if [ ! -f ./ca/ca_certs/cert.pem ]; then
     openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes -subj "/C=US/ST=private/L=province/O=city/CN=hostname.example.com"
     popd
 fi
-cp -r ./ca/ca_certs ./testing_against_browsers
+cp -r ./ca/ca_certs ./testing_against_browsers/chrome
 
-pushd ./testing_against_browsers
-sudo docker build . --tag browser_testing:latest
+pushd ./testing_against_browsers/chrome
+sudo docker build . --tag chrome_testing:latest
 popd
 
 # build first to make `cargo run` happen really quickly ;)
@@ -21,4 +21,4 @@ cargo run -- mitm -p 8080 &
 echo "Sleeping to let third-wheel start running"
 sleep 1
 
-sudo docker run --network host browser_testing:latest
+sudo docker run --rm --network host chrome_testing:latest
