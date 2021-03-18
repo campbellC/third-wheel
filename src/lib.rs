78540f824c29de6466242fbe8ceb7945daf2d4cd
Chris Campbell <campbellC@users.noreply.github.com>
2021-03-18 10:06:40 +0000

    Bump MV

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@
 //! between clients and servers. Client code needs to provide a Layer that
 //! constructs a Service for intercepting requests and responses. `mitm_layer`
 //! provides a convenience function for producing these easily.
+//!
+//! The best way to see how to use this crate is to take a look at the examples.
 
 //Rustc lints
 //<https://doc.rust-lang.org/rustc/lints/listing/allowed-by-default.html>
